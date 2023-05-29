9aa7eaaaf071025850839a62f4d7911ee2facb46
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-29 23:13:50 +0200

    température3

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,12 +1,12 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
     let mut celsius : f64=0.0;
-    celsius=(f-32.0)/1.8
+    celsius=(f - 32.0) * (5.0 / 9.0)
     
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
     let mut fahrenheit : f64 =0.0;
-    fahrenheit=(c*1.8)+32.0
+    fahrenheit=(c * (9.0 / 5.0)) + 32.0
     //°F =( °C x 1,8 ) + 32
     
 }
