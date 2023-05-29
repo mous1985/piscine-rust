d4f284310baf84f4c6ebc8724acc609a811612f0
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 00:52:43 +0200

    stp

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,9 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    
-    (f - 32.0) * (9.0 / (5.0))
-    
-    
+    let result = (f - 32.0) * (9.0 / 5.0);
+    println!("{:.16}", result); // Affiche le résultat avec une précision de 16 chiffres après la virgule
+    result
 }
+
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
    
