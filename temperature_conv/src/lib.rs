bf1f9445a3312cbae10b1617631b6fb7e7017c29
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 02:51:40 +0200

    jesper

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,5 +1,7 @@
+use fixed::types::I10F22;
+
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (5.0 / 9.0)
+    ((f - 32.0) * (5.0 / 9.0)).to_fixed(2);
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
