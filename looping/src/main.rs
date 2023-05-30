2922828425342253849a2765d0d67905d13d85aa
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 11:40:48 +0200

    oueoueoue

--- looping/src/main.rs
+++ looping/src/main.rs
@@ -1,29 +1,23 @@
-use std::io;
+use std::io::{self, Write};
 
 fn main() {
+    let riddle = "I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?";
+    let answer = "The letter e";
     let mut tries = 0;
-    let answer = "e";
-
-    println!("Riddle: I am the beginning of the end, and the end of time and space. I am essential to creation, and I surround every place. What am I?");
 
     loop {
-        let mut guess = String::new();
+        println!("{}", riddle);
+        io::stdout().flush().expect("Failed to flush stdout.");
 
-        io::stdin()
-            .read_line(&mut guess)
-            .expect("Error reading input.");
-
-        let guess = guess.trim();
+        let mut user_input = String::new();
+        io::stdin().read_line(&mut user_input).expect("Failed to read line.");
 
         tries += 1;
 
-        if guess == answer {
-            println!("Correct answer!");
+        if user_input.trim() == answer {
             break;
-        } else {
-            println!("Wrong answer. Try again.");
         }
     }
 
-    println!("Number of tries needed: {}", tries);
+    println!("Number of trials: {}", tries);
 }
