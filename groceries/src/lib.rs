efac90e62a53a0f32f6ce579d811c274c46e497e
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 12:33:01 +0200

    yawy

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -2,6 +2,6 @@ pub fn insert(vec: &mut Vec<String>, val: String) {
     vec.push(val)
 }
 
-pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+pub fn at_index(vec: &Vec<String>, index: usize) -> Option<&String> {
     vec.get(index)
 }
