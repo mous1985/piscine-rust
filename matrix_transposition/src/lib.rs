a3efbcccefd239b4cc463a83207eb46f526f0d03
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 22:39:37 +0200

    hh

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -3,7 +3,7 @@
 pub struct Matrix((i32, i32), (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    let ((a, b), (c, d)) = m;
+    let ((a, b), (c, d)) = m.0;
     Matrix((a, c), (b, d))
 }
 
