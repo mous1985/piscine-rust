5dddf4f319f51755265c483c9dc4d121ef1d2774
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 22:33:07 +0200

    pitertre

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -7,8 +7,4 @@ pub fn transpose(m: Matrix) -> Matrix {
     Matrix((a, c), (b, d))
 }
 
-fn main() {
-    let matrix = Matrix((1, 3), (4, 5));
-    println!("Original matrix {:?}", matrix);
-    println!("Transpose matrix {:?}", transpose(matrix));
-}
+
