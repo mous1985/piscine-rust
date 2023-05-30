a0ad343b91e0bed1001ee26183c4617c7b147a23
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 02:11:31 +0200

    rassi

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,13 +1,9 @@
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    let celsius = (f - 32.0) * (5.0 / 9.0);
-    let rounded_celsius = (celsius * 100.0).round() / 100.0;
-    rounded_celsius
+    ((f - 32.0) * (5.0 / 9.0)).to_fixed(2)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    let fahrenheit = (c * (9.0 / 5.0)) + 32.0;
-    let rounded_fahrenheit = (fahrenheit * 100.0).round() / 100.0;
-    rounded_fahrenheit
+    ((c * (9.0 / 5.0)) + 32.0).to_fixed(2)
 }
 
     
