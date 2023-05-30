923b7d9b33923168ae6fb3a5a13bfbc21d58e6e2
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-31 00:34:11 +0200

    cest le bon

--- matrix_transposition/src/lib.rs
+++ matrix_transposition/src/lib.rs
@@ -1,9 +1,10 @@
 
 #[derive(Debug, PartialEq, Eq)]
-pub struct Matrix((i32, i32), (i32, i32));
+pub struct Matrix(pub(i32, i32),pub (i32, i32));
 
 pub fn transpose(m: Matrix) -> Matrix {
-    let ((a, b), (c, d)) = m.0;
+    let (a, b) = m.0;
+    let (c, d) = m.1;
     Matrix((a, c), (b, d))
 }
 
