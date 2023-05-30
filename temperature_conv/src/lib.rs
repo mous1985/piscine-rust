bb6735d70bb2b701179690ccf19a997c5d4309b7
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 03:40:27 +0200

    gggggg

--- temperature_conv/src/lib.rs
+++ temperature_conv/src/lib.rs
@@ -1,10 +1,10 @@
 
 pub fn fahrenheit_to_celsius(f: f64) -> f64 {
-    (f - 32.0) * (1.0/(9.0 / 5.0))
+    (f - 32.0) * (1.0/1.8)
 }
 
 pub fn celsius_to_fahrenheit(c: f64) -> f64 {
-    (c * (9.0 / 5.0)) + 32.0
+    (c * 1.8) + 32.0
 }
 
     
