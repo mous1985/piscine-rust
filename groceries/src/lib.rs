1b4a726a650400ba6838b2539a02188ab562e69a
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-30 12:35:28 +0200

    ca casse la tete

--- groceries/src/lib.rs
+++ groceries/src/lib.rs
@@ -1,7 +1,7 @@
 pub fn insert(vec: &mut Vec<String>, val: String) {
-    vec.push(val)
+    vec.push(val);
 }
 
-pub fn at_index(vec: &Vec<String>, index: usize) -> Option<&String> {
-    vec.get(index)
+pub fn at_index(vec: &Vec<String>, index: usize) -> String {
+    vec[index].clone()
 }
