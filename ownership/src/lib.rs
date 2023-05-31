ddb14beea9d7c8b059cb202c4f7ace348cd17f29
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-05-31 10:33:37 +0200

    regardé tout

--- ownership/src/lib.rs
+++ ownership/src/lib.rs
@@ -1,16 +1,16 @@
-pub fn first_subword(mut s: String) -> String {
-    // Trouver la position du premier caractère en majuscule ou du caractère de soulignement
-    let first_upper_or_underscore = s
-        .find(|c: char| c.is_ascii_uppercase() || c == '_')
-        .unwrap_or_else(|| s.len());
+pub fn first_subword(s: String) -> String {
+    let mut subword = String::new();
 
-    // Extraire la première sous-chaîne jusqu'à cette position
-    let subword: String = s.drain(..first_upper_or_underscore).collect();
-
-    // Si la sous-chaîne extraite est vide, renvoyer la chaîne originale
-    if subword.is_empty() {
-        s
-    } else {
-        subword
+    for c in s.chars() {
+        if c.is_ascii_uppercase() || c == '_' {
+            if !subword.is_empty() {
+                break;
+            }
+        }
+        if c != '_' {
+            subword.push(c);
+        }
     }
+
+    subword
 }
