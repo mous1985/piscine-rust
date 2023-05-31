9a3e89075c5f6017c2410d54eaf37fdd8c604d28
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-01 01:02:05 +0200

    okokok

---
+++ changes/src/lib.rs
@@ -0,0 +1,25 @@
+#[derive(Debug, Eq, PartialEq, Clone)]
+pub struct Light {
+	pub alias: String,
+	pub brightness: u8,
+}
+
+pub fn new(alias: &str) -> Self {
+    impl Light {
+            pub fn new(alias: &str) -> Self {
+                Self {
+                    alias: alias.to_string(),
+                    brightness: 0,
+                }
+            }
+	}
+}
+
+pub fn change_brightness(lights: &mut Vec<Light>, alias: &str, value: u8) {
+    for light in lights.iter_mut() {
+        if light.alias == alias {
+            light.brightness = value;
+            break;
+        }
+    }
+}
