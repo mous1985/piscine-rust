cd1c50e7198f597b21028af4ef794b6e55bae630
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-03 00:39:22 +0200

    mlk

--- strings/src/lib.rs
+++ strings/src/lib.rs
@@ -1,3 +1,23 @@
-pub fn char_length(s: &str) -> usize {
-    s.chars().count()
+pub fn edit_distance(source: &str, target: &str) -> usize {
+    let n = source.chars().count();
+    let m = target.chars().count();
+
+    let mut dp = vec![vec![0; m + 1]; n + 1];
+
+    for i in 0..=n {
+        dp[i][0] = i;
+    }
+
+    for j in 0..=m {
+        dp[0][j] = j;
+    }
+
+    for (i, source_char) in source.chars().enumerate() {
+        for (j, target_char) in target.chars().enumerate() {
+            let cost = if source_char == target_char { 0 } else { 1 };
+            dp[i + 1][j + 1] = dp[i][j + 1].min(dp[i + 1][j]).min(dp[i][j] + cost);
+        }
+    }
+
+    dp[n][m]
 }
