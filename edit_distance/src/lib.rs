521fa1ef9a6dea9f6a10da7abb0c76258b3f92d5
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-03 00:47:22 +0200

    bbv

--- edit_distance/src/lib.rs
+++ edit_distance/src/lib.rs
@@ -1,23 +1,32 @@
 pub fn edit_distance(source: &str, target: &str) -> usize {
-    let n = source.chars().count();
-    let m = target.chars().count();
-
-    let mut dp = vec![vec![0; m + 1]; n + 1];
-
-    for i in 0..=n {
-        dp[i][0] = i;
+    let mut matrix = vec![vec![0; target.len() + 1]; source.len() + 1];
+    
+    for i in 0..=source.len() {
+        matrix[i][0] = i;
+    }    
+    for j in 0..=target.len() {
+        matrix[0][j] = j;
     }
-
-    for j in 0..=m {
-        dp[0][j] = j;
-    }
-
-    for (i, source_char) in source.chars().enumerate() {
-        for (j, target_char) in target.chars().enumerate() {
-            let cost = if source_char == target_char { 0 } else { 1 };
-            dp[i + 1][j + 1] = dp[i][j + 1].min(dp[i + 1][j]).min(dp[i][j] + cost);
+    
+    
+    for i in 1..=source.len() {
+        for j in 1..=target.len() {
+            
+            let cost = if source.chars().nth(i - 1) == target.chars().nth(j - 1) {
+                0
+            } else {
+                1
+            };
+    
+            matrix[i][j] = std::cmp::min(
+                matrix[i - 1][j] + 1,
+                std::cmp::min(
+                    matrix[i][j - 1] + 1,
+                    matrix[i - 1][j - 1] + cost
+                )
+            );
         }
     }
-
-    dp[n][m]
+    
+    matrix[source.len()][target.len()]
 }
