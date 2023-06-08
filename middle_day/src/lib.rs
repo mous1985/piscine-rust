9e8f5106a03cffac9db6c86117003dbc1c92deea
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-08 17:15:38 +0200

    second commit

--- middle_day/src/lib.rs
+++ middle_day/src/lib.rs
@@ -1,10 +1,14 @@
-use chrono::{NaiveDate, Weekday};
+extern crate chrono;
+use chrono::{Datelike, NaiveDate, Weekday};
 
-pub fn middle_day(year: i32) -> Option<Weekday> {
-    let first_day = NaiveDate::from_ymd(year, 1, 1);
-    let last_day = NaiveDate::from_ymd(year, 12, 31);
+pub type wd = Weekday;
+
+pub fn middle_day(year: i32) -> Option<wd> {
+    let first_day = NaiveDate::from_ymd_opt(year, 1, 1)?;
+    let last_day = NaiveDate::from_ymd_opt(year, 12, 31)?;
 
     let total_days = last_day.signed_duration_since(first_day).num_days();
+
     if total_days % 2 == 0 {
         return None;
     }
