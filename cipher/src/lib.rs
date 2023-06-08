4d9363771d9ef858e8228e293ee965474069b163
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-08 03:51:25 +0200

    enfin

---
+++ cipher/src/lib.rs
@@ -0,0 +1,35 @@
+#[derive(Debug, Clone, Eq, PartialEq)]
+pub struct CipherError {
+    pub validation: bool,
+    pub expected: String,
+}
+
+impl CipherError {
+    pub fn new(validation: bool, expected: String) -> CipherError {
+        CipherError {
+            validation,
+            expected,
+        }
+    }
+}
+
+pub fn cipher(original: &str, ciphered: &str) -> Result<Result<bool, CipherError>, ()> {
+    if ciphered.is_empty() || original.is_empty() {
+        return Err(());
+    }
+
+    let expected = original
+        .chars()
+        .map(|c| match c {
+            'a'..='z' => (b'z' - (c as u8 - b'a')) as char,
+            'A'..='Z' => (b'Z' - (c as u8 - b'A')) as char,
+            _ => c,
+        })
+        .collect::<String>();
+
+    if expected == ciphered {
+        Ok(Ok(true))
+    } else {
+        Ok(Err(CipherError::new(false, expected)))
+    }
+}
