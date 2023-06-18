39bb813598f52b7dec1492deeda9e375b64148e1
mus_36 <mustapha.benazzouz.213@gmail.com>
2023-06-18 05:24:31 +0200

    récursivité

---
+++ box_recursion/src/lib.rs
@@ -0,0 +1,41 @@
+#[derive(Debug)]
+pub struct WorkEnvironment {
+    pub grade: Link,
+}
+
+pub type Link =Option<Box<Worker>>;
+
+#[derive(Debug)]
+pub struct Worker {
+    pub role: String,
+    pub name: String,
+    pub next: Link,
+}
+
+impl WorkEnvironment {
+    pub fn new() -> WorkEnvironment {
+        let workEnv=WorkEnvironment{
+            grade:none,
+        }
+    }
+    pub fn add_worker(&mut self, role: String, name: String) {
+        let worker = Worker {
+            role,
+            name,
+            next: self.grade.take(),
+        };
+        self.grade = Some(Box::new(worker));
+    }
+    pub fn remove_worker(&mut self) -> Option<String> {
+        if let Some(mut boxed_worker) = self.grade.take() {
+            let worker_name = boxed_worker.name.clone();
+            self.grade = boxed_worker.next.take();
+            Some(worker_name)
+        } else {
+            None
+        }
+    }
+    pub fn last_worker(&self) -> Option<(String, String)> {
+        self.grade.as_ref().map(|worker| (worker.name.clone(), worker.role.clone()))
+    }
+}
